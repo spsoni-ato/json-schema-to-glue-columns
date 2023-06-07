b61a2793a88fa19e95b51d9df870534c5fa54e16
Sai <80665633+sai-poona@users.noreply.github.com>
2023-06-07 16:00:35 +1000

    Add helper functions to replace placeholders and create partition keys

--- json_schema_to_glue_columns.py
+++ json_schema_to_glue_columns.py
@@ -1,7 +1,9 @@
 import json
+import re
 
 import boto3
 import yaml
+
 
 def load_json_schema(schema_file_location):
     """
@@ -89,6 +91,64 @@ def load_yaml_from_s3(bucket_name, object_key):
     yaml_data = yaml.safe_load(yaml_content)
 
     return yaml_data
+
+
+def replace_placeholders(dictionary, placeholders):
+    """
+    Replaces placeholders in a dictionary with corresponding values using regex.
+
+    Args:
+        dictionary (dict): The dictionary to process.
+        placeholders (dict): A dictionary of placeholders and their corresponding values.
+
+    Returns:
+        dict: The updated dictionary with replaced placeholders.
+    """
+    replaced_dict = {}
+    for key, value in dictionary.items():
+        if isinstance(value, dict):
+            # Recursively process nested dictionaries
+            replaced_dict[key] = replace_placeholders(value, placeholders)
+        elif isinstance(value, str):
+            # Replace placeholders in string values using regex
+            pattern = re.compile(r'\[(.*?)\]')
+            replaced_value = pattern.sub(lambda x: placeholders[x.group()], value)
+            replaced_dict[key] = replaced_value
+        else:
+            # Keep non-string, non-dict values as is
+            replaced_dict[key] = value
+    return replaced_dict
+
+
+def create_partition_keys(partition_key_list):
+    """
+    Creates a list of partition keys from a given partition key list.
+
+    Args:
+        partition_key_list (list): A list of partition keys in the format "key_name:key_type".
+
+    Returns:
+        list: A list of partition keys as dictionaries, with each dictionary containing "Name" and "Type" keys.
+
+    Example:
+        Input: ["year:int", "month:string"]
+        Output: [{"Name": "year", "Type": "int"}, {"Name": "month", "Type": "string"}]
+    """
+    partition_keys = []
+    for partition_key in partition_key_list:
+        # Split the partition key into name and type
+        key_name, key_type = partition_key.split(":")
+
+        # Create a dictionary for the partition key
+        partition_key_dict = {
+            "Name": key_name.strip(),
+            "Type": key_type.strip()
+        }
+
+        # Add the partition key to the list
+        partition_keys.append(partition_key_dict)
+
+    return partition_keys
 
 
 def flatten_json_schema(json_schema, prefix='', delimiter='_'):
